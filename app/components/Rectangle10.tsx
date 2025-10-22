b24740baeca609e3da43b6059bedcefdbb9c1c2b
Tai-DT <taidt3004@gmail.com>
2025-10-22 20:20:56 +0700

    feat: remove entire frontend codebase

--- app/components/Rectangle10.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next';
-
-const Rectangle10: NextPage = () => {
-  return (
-    <div className="w-full h-[717px] relative rounded-[60px] bg-white" />
-  );
-};
-
-export default Rectangle10;
